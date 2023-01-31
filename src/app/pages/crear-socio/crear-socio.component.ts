8d5faef712e2e2d56162fbc660c6f4361414de5c
psidrovo <paulsidrovob@gmail.com>
2023-01-31 14:04:30 -0500

    Crear-socio

---
+++ src/app/pages/crear-socio/crear-socio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-socio',
+  templateUrl: './crear-socio.component.html',
+  styleUrls: ['./crear-socio.component.css']
+})
+export class CrearSocioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
